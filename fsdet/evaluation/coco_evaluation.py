b672b30d87cf601f89e77d5cb5f858232da45ca4
Idris <iawardere3@gmail.com>
2023-04-20 15:30:10 -0400

    Adjusted COCOEvaluator for custom dataset

--- fsdet/evaluation/coco_evaluation.py
+++ fsdet/evaluation/coco_evaluation.py
@@ -61,11 +61,14 @@ class COCOEvaluator(DatasetEvaluator):
             or "novel" in dataset_name
         )
         # fmt: off
+        # self._base_classes = [
+        #     8, 10, 11, 13, 14, 15, 22, 23, 24, 25, 27, 28, 31, 32, 33, 34, 35,
+        #     36, 37, 38, 39, 40, 41, 42, 43, 46, 47, 48, 49, 50, 51, 52, 53, 54,
+        #     55, 56, 57, 58, 59, 60, 61, 65, 70, 73, 74, 75, 76, 77, 78, 79, 80,
+        #     81, 82, 84, 85, 86, 87, 88, 89, 90,
+        # ]
         self._base_classes = [
-            8, 10, 11, 13, 14, 15, 22, 23, 24, 25, 27, 28, 31, 32, 33, 34, 35,
-            36, 37, 38, 39, 40, 41, 42, 43, 46, 47, 48, 49, 50, 51, 52, 53, 54,
-            55, 56, 57, 58, 59, 60, 61, 65, 70, 73, 74, 75, 76, 77, 78, 79, 80,
-            81, 82, 84, 85, 86, 87, 88, 89, 90,
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 # Represents the 10 valorant agents (and one dummy class from dataset)
         ]
         # fmt: on
 
@@ -147,6 +150,9 @@ class COCOEvaluator(DatasetEvaluator):
                 for k, v in self._metadata.thing_dataset_id_to_contiguous_id.items()
             }
             for result in self._coco_results:
+                # Fixing too many classes issue
+                if result["category_id"] > 10:
+                    result["category_id"] = 0
                 result["category_id"] = reverse_id_mapping[
                     result["category_id"]
                 ]
