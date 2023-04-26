0fee580dc7ecc6b27d28bd0456adf3cc5ac9f326
Du Huang <duhuang@andrew.cmu.edu>
2023-04-25 21:44:26 -0700

    Update

--- fsdet/engine/defaults.py
+++ fsdet/engine/defaults.py
@@ -471,6 +471,37 @@ class DefaultTrainer(SimpleTrainer):
         Overwrite it if you'd like a different optimizer.
         """
         return build_optimizer(cfg, model)
+        
+        lr = 0.001
+        layer_decay_factor = 0.5
+        decay_num = 10
+        parameters = []
+        layer_names = []
+        for idx, (name, param) in enumerate(model.named_parameters()):
+            layer_names.append(name)
+        prev_group_name = layer_names[-1].split('.')[-2]
+        for name in layer_names[::-1]:
+            # # parameter group name
+            cur_group_name = name.split('.')[-2]
+            #print(cur_group_name)
+            # # update learning rate
+            if cur_group_name != prev_group_name:
+                lr *= layer_decay_factor
+                decay_num -= 1
+            prev_group_name = cur_group_name
+            # # display info
+            # # append layer parameters # order problem !!
+            parameters.append({'params': [p for n, p in model.named_parameters() if n == name ],
+                            'lr':     lr,
+                            'dampening': 0,
+                            'momentum': 0.9,
+                            'weight_decay': 0.0001,
+                            'foreach': True})
+            if decay_num == 0: break
+        optimizer = torch.optim.SGD(parameters)
+        del parameters
+        return optimizer
+        
 
     @classmethod
     def build_lr_scheduler(cls, cfg, optimizer):
